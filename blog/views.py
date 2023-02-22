3c2bcf25b56184d4edf1217135a062ddbe103789
om04an <mivanpass@gmail.com>
2023-02-22 07:17:53 +0300

    stage connection db with frontend

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import User, Profile
 
 
 def home(request):
-    return render(request, 'index.html')
+    data_user = ''
+    if request.user.is_authenticated:
+        user = request.user
+        data_user = Profile.objects.get(user=user)
+    
+    return render(request, 'index.html', {'data_user': data_user})
