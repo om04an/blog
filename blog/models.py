d9771c10335a75dce275d023a85bf9095931ec06
om04an <mivanpass@gmail.com>
2023-02-24 17:47:31 +0300

    Update db post and add funcion to html

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,19 @@ from django_resized import ResizedImageField
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_photo = ResizedImageField(size=[300, 300], crop=['middle', 'center'], upload_to='static/images/avatar', null=True, default='static/images/avatar/main.jpg')
     number_of_posts = models.IntegerField()
 
     def __str__(self):
         return str(self.user)
 
+class Post(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    date_create = models.DateField(auto_now_add=True)
+    number_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.user) + ' | ' + str(self.title)[:60] + '...'
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-from .models import User, Profile
+from .models import User, Profile, Post
 
 
 def home(request):
     data_user = ''
     if request.user.is_authenticated:
-        user = request.user
-        data_user = Profile.objects.get(user=user)
-    
-    return render(request, 'index.html', {'data_user': data_user})
+        data_user = _getting_user_data(request)
+    posts = Post.objects.all()
+
+    return render(request, 'index.html', {'data_user': data_user,
+                                          'posts': posts,
+                                          })
+
+
+def _getting_user_data(request):
+    nickname = request.user     # obtaining a nickname of an authorized user
+
+    if not Profile.objects.filter(user=nickname):   # check there is an entry in the profile table and if not, then a default entry is added
+        user = Profile(user=nickname, number_of_posts=0)
+        user.save()
+
+    data_user = Profile.objects.get(user=nickname)
+
+    return data_user
